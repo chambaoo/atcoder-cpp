071491863344af048ac03e1162f1a9894e29824c
chambaoo <chambao752@gmail.com>
2026-02-14 20:58:19 +0900

    backup: study/20260214/wip

--- study/training/easy/260213-1600/d/main.cpp
+++ study/training/easy/260213-1600/d/main.cpp
@@ -7,6 +7,14 @@ int main()
     int h, w;
     cin >> h >> w;
 
+    // 8方向に見つけるための設定
+    // 0 1 2
+    // 3   4
+    // 5 6 7
+    // このとき、上下方向（i）, 横方向（j）がどれだけ変化するか？を表す配列2つ
+    int di[] = {-1, -1, -1, 0, 0, 1, 1, 1};
+    int dj[] = {-1, 0, 1, -1, 1, -1, 0, 1};
+
     // 1行ずつ入力を受け付ける
     vector<string> s(h);
 
@@ -14,20 +22,33 @@ int main()
 
     string SNUKE = "snuke";
 
-    // 右方向に見つけるまではこれでOK.
-    // TODO: 8方向すべてに対応させる
     rep (i, h) rep (j, w) {
 
-        rep (k, 5) {
-            // 探す範囲外になったらやめる
-            if (j >= w) break;
+        // 8方向にチェックする
+        rep (v, 8) {
+            int ni = i;
+            int nj = j;
 
-            // 探している文字列と違う文字が現れたらやめる
-            if (s[i][j] != SNUKE[k]) break;
-            if (k == 4) {
-                //found
+            // 探している文字列であるかどうかを調べる
+            rep (k, 5) {
+                // 探す範囲外になったらやめる
+                if (i < 0 || j < 0) break;
+                if (i >= h || j >= w) break;
+
+                // 探している文字列と違う文字が現れたらやめる
+                if (s[i][j] != SNUKE[k]) break;
+                if (k == 4) {
+                    ni = i;
+                    nj = j;
+                    // 見つかったとき
+                    rep (nk, 5) {
+                        cout << i + 1 << " " << j + 1 << endl;
+                    }
+                    return 0;
+                }
+                ni += di[v];
+                nj += dj[v];
             }
-            j++;
         }
     }
 

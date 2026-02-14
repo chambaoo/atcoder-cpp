048a003121c7f13181ef21a098215c9a52647cef
chambaoo <chambao752@gmail.com>
2026-02-14 20:35:38 +0900

    backup: study/20260214

--- study/training/easy/260213-1600/d/main.cpp
+++ study/training/easy/260213-1600/d/main.cpp
@@ -1,41 +1,33 @@
 #include <bits/stdc++.h>
 using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int main()
 {
-    int n, m, x, y;
-    int dx[8] = {-1, -1, -1, 0, 0, 1, 1, 1};
-    int dy[8] = {-1, 0, 1, -1, 1, -1, 0, 1};
-    string str;
+    int h, w;
+    cin >> h >> w;
 
-    cin >> n >> m;
-    vector<string> s(n);
-    for (int i = 0; i < n; i++)
-        cin >> s[i];
-    for (int i = 0; i < n; i++)
-    {
-        for (int j = 0; j < m; j++)
-        {
-            for (int k = 0; k < 8; k++)
-            {
-                str = "";
-                for (int t = 0; t < 5; t++)
-                {
-                    x = i + t * dx[k], y = j + t * dy[k];
-                    if ((x < 0) || (x >= n) || (y < 0) || (y >= m))
-                        break;
-                    str += s[x][y];
-                }
-                if (str == "snuke")
-                {
-                    for (int t = 0; t < 5; t++)
-                    {
-                        x = i + t * dx[k] + 1, y = j + t * dy[k] + 1;
-                        cout << x << " " << y << endl;
-                    }
-                    return 0;
-                }
+    // 1行ずつ入力を受け付ける
+    vector<string> s(h);
+
+    rep (i, h) cin >> s.at(i);
+
+    string SNUKE = "snuke";
+
+    // 右方向に見つけるまではこれでOK.
+    // TODO: 8方向すべてに対応させる
+    rep (i, h) rep (j, w) {
+
+        rep (k, 5) {
+            // 探す範囲外になったらやめる
+            if (j >= w) break;
+
+            // 探している文字列と違う文字が現れたらやめる
+            if (s[i][j] != SNUKE[k]) break;
+            if (k == 4) {
+                //found
             }
+            j++;
         }
     }
 

b33757bbbcca10f11731392290f1ff86d1826cf5
chambaoo <chambao752@gmail.com>
2026-02-16 17:55:02 +0900

    add: todo study/20260216

---
+++ study/training/easy/260216-1600/e/main.cpp
@@ -0,0 +1,9 @@
+// https://atcoder.jp/contests/adt_easy_20260216_1/tasks/abc307_c
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main() {
+    // todo
+    return 0;
+}

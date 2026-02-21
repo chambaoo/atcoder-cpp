2ce9d3e20d75ce01079e25731d64b2982645186a
chambaoo <chambao752@gmail.com>
2026-02-21 22:47:32 +0900

    add: code snippet char type

--- lib/snippet/char.cpp
+++ lib/snippet/char.cpp
@@ -3,25 +3,14 @@ using namespace std;
 #define rep(i, n) for (int i = 0; i < (n); ++i)
 
 int main() {
-    string str;
-    cin >> str;
-    
-    int count_lower = 0;
-    string upper = "";
-    string lower = "";
 
-    rep(i, str.length()) {
-        if (islower(str.at(i))) {
-            count_lower++;
-            lower += str.at(i);
-            upper += toupper(str.at(i));
-        } else {
-            lower += tolower(str.at(i));
-            upper += str.at(i);
-        }
-    }
-    if (count_lower > str.length() / 2) cout << lower << endl;
-    else cout << upper << endl;
+    char C = 'C';
+    bool is_lower = islower(C); // false
+    char cl = tolower(C);
+
+    char c = 'c';
+    bool is_upper = isupper(c); // false
+    char cu = toupper(c);
 
     return 0;
 }

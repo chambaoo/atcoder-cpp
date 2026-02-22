ab9577082e79e12402682af580a13fcc179feb82
chambaoo <chambao752@gmail.com>
2026-02-22 18:57:46 +0900

    add: code snippet queue

---
+++ lib/snippet/queue.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+
+int main(void) {
+    
+    queue<int> q;
+    
+    int i = 10;
+    q.push(i);
+    q.front();
+    q.pop();
+    q.empty();
+    q.size();
+
+    return 0;
+}

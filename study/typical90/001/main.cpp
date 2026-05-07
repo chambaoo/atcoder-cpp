72d9532b12361fd5523fa81682ea2db225f0b59b
chambaoo <chambao752@gmail.com>
2026-05-07 18:21:30 +0900

    add: study/20260507

---
+++ study/typical90/001/main.cpp
@@ -0,0 +1,57 @@
+// https://atcoder.jp/contests/typical90/tasks/typical90_a
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
+
+int main() {
+    ll N, L, K;
+    cin >> N >> L >> K;
+
+    vector<ll> A(N);
+    rep(i, N) cin >> A[i];
+
+    // 判定問題
+    // すべての長さを x 以上にすることが可能か？
+    // bool check(ll x) のような関数を、ラムダ式で書いているよ
+    auto check = [&](ll x) -> bool {
+        // 長さ x 以上のピースを何個作れたか
+        ll num = 0;
+        // 前回切った位置
+        ll pre = 0;
+        for (int i = 0; i < N; ++i) {
+            // x を超えたら切断
+            // 最短で条件を満たした瞬間に切る(貪欲法)
+            if (A[i] - pre >= x) {
+                ++num;
+                pre = A[i];
+            }
+        }
+        // 最後の切れ目まではループで処理しているが、
+        // 最後の切れ目から羊羹の末端までのピースだけ未判定で残っている
+        // 最後のピースが x 以上なら加算
+        if (L - pre >= x) ++num;
+
+        // K+1 個以上作れたなら、trueを返す
+        return (num >= K + 1);
+    };
+
+    // 二分探索
+    // left = OK が確定している位置
+    // right = NG が確定している位置
+    ll left = -1, right = L + 1;
+
+    // OK と NG の境目を探す
+    while (right - left > 1) {
+        // mid = 答え候補
+        ll mid = (left + right) / 2;
+        // 候補の数で、長さを作れたら、OKの位置を更新する
+        if (check(mid)) left = mid;
+        // 候補の数で、長さを作れなかったら、NGの位置を更新する
+        else right = mid;
+    }
+    cout << left << endl;
+
+
+    return 0;
+}

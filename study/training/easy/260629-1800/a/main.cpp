14352b0df7b08a96eb798b615aaa0052b8d546a5
chambaoo <chambao752@gmail.com>
2026-07-04 15:01:23 +0000

    add: study/20260704

---
+++ study/training/easy/260629-1800/a/main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
+
+int main() {
+    int n, s;
+    cin >> n >> s;
+
+
+    int prev = 0;
+
+    rep (i, n) {
+        int t;
+        cin >> t;
+
+        if (t - prev > s) {
+            cout << "No" << endl;
+            return 0;
+        }
+
+        prev = t;
+    }
+
+    cout << "Yes" << endl;
+
+    return 0;
+}
+
+// --------------------------------
+// Reference
+// --------------------------------

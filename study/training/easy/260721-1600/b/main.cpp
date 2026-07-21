6f4641d336cbcb9ce32716addcb27f4e2851e269
chambaoo <chambao752@gmail.com>
2026-07-21 13:23:58 +0000

    add: study

---
+++ study/training/easy/260721-1600/b/main.cpp
@@ -0,0 +1,22 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
+
+int main() {
+    int n;
+    cin >> n;
+
+    int sum = 0;
+    rep (i, n) {
+        sum += i + 1;
+    }
+
+    cout << sum << endl;
+
+    return 0;
+}
+
+// --------------------------------
+// Reference
+// --------------------------------

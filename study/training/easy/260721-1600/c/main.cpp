8b7f2352dfacd61871cc20af6e1d4351efbd95f4
chambaoo <chambao752@gmail.com>
2026-07-21 14:34:27 +0000

    add: study

---
+++ study/training/easy/260721-1600/c/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define ll long long
+
+int main() {
+    int n;
+    cin >> n;
+
+    int p = 5 * n;
+    
+    vector<int> x(p);
+
+    rep (i, p) {
+        cin >> x[i];
+    }
+
+    sort(x.begin(), x.end());
+
+    double score = 0;
+    rep (i, p) {
+        if (i < n || i >= p - n) continue;
+        score += x[i];
+    }
+
+    cout << score / (3 * n) << endl;
+    
+
+    return 0;
+}
+
+// --------------------------------
+// Reference
+// --------------------------------
